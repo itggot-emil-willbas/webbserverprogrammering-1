536340a328a32002d4e4b20ca160856429d934e3
Daniel Berg <daniel.berg@it-gymnasiet.se>
2017-09-04 16:23:41 +0200

    Flyttade runt bild- och include-mappar för att fungera med build-scriptet

--- 04_Databasmodellering_med_ER/render.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'asciidoctor' 
-require 'asciidoctor/cli/options'
-require 'asciidoctor/cli/invoker'
-
-require 'asciidoctor-diagram' 
-
-Asciidoctor.convert_file ARGV[0], to_file: true, safe: :safe
